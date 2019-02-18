68d0f8e892c74f4265a691f1cf1b48bbe31b5e7d
Nim <areuschle1@gmail.com>
2019-02-18 15:17:11 -0600

    plugin action

--- src/Main.server.lua
+++ src/Main.server.lua
@@ -2,15 +2,27 @@
 -- Select the parent of current selection
 
 local Selection = game:GetService("Selection")
+local HistoryService = game:GetService("ChangeHistoryService")
 
 local source = script.Parent
 
 local Config = require(source:WaitForChild("Config"))
 
 local pluginToolbar = plugin:CreateToolbar(Config.PLUGIN_NAME)
-local pluginButton = pluginToolbar:CreateButton(Config.PLUGIN_NAME,Config.PLUGIN_DESC,Config.PLUGIN_ICON)
+local pluginButton = pluginToolbar:CreateButton(
+    Config.PLUGIN_NAME,
+    Config.PLUGIN_DESC,
+    Config.PLUGIN_ICON
+)
+local pluginAction = plugin:CreatePluginAction(
+    Config.PLUGIN_ID.."-action",
+    Config.PLUGIN_DESC,
+    Config.PLUGIN_DESC,
+    Config.PLUGIN_ICON
+)
 
 local function selectParents()
+
     local childrenSelection = Selection:Get()
 
     local parentsIndex = {}
@@ -25,7 +37,12 @@ local function selectParents()
         end
     end
 
+    HistoryService:SetWaypoint("Selecting parents")
     Selection:Set(newParents)
+    HistoryService:SetWaypoint("Parents selected")
 end
 
+HistoryService:SetEnabled(true)
+
+pluginAction.Triggered:Connect(selectParents)
 pluginButton.Click:Connect(selectParents)
