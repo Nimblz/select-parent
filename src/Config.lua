dbd4e57a5ef876829a0e52d087bf3cbece47f9c2
Nim <areuschle1@gmail.com>
2019-02-18 14:24:18 -0600

    plugin configuration

---
+++ src/Config.lua
@@ -0,0 +1,6 @@
+return {
+    PLUGIN_NAME =   "SelectParent",
+    PLUGIN_DESC =   "Selects the parent of current selection."..
+                    "If multiple objects are selected, all parents are selected.",
+    PLUGIN_ICON =   "rbxassetid://0",
+}
